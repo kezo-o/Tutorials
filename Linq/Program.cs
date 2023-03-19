af32c7c5383c5209e0bfc7c3b6134629ad321b72
HansaJugnarain <48100071+HansaJugnarain@users.noreply.github.com>
2023-03-19 17:28:41 +0400

    Test checkin

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -12,6 +12,7 @@ namespace Linq
         {
             var studentIds = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             studentIds.OrderBy(o => o);
+            studentIds.OrderByDescending(o => o);
             Console.ReadKey();
         }
     }
