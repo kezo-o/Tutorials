58c1f97fe97f32fe7a21ad2fb184f837420900b7
kezo-o <61119265+kezo-o@users.noreply.github.com>
2023-03-20 20:58:06 +0400

    Exercise 1 Generics

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -11,12 +11,36 @@ namespace Generics
         static void Main(string[] args)
         {
 
+            bool IsEqual;
+            IsEqual = ClsCalculator.AreEqual(10, 20);
+            PrintMessage(IsEqual);
+            IsEqual = ClsCalculator.AreEqual<string>("ABC", "ABC");
+            IsEqual = ClsCalculator.AreEqual<double>(10.5, 20.5);
+
+           
+
+            Console.ReadKey();
 
         }
 
-        static void Add<T>(T obj)
+        private static void PrintMessage(bool IsEqual)
         {
+            if (IsEqual)
+                Console.WriteLine("Both are Equal");
+            else
+                Console.WriteLine("Both are Not Equal");
+        }
 
+
+    }
+
+    //Exercise 1 rewrite the AreEqual method 
+    //using generics to make all 3 methods work
+    public class ClsCalculator
+    {
+        public static bool AreEqual(int value1, int value2)
+        {
+            return value1 == value2;
         }
     }
 }
