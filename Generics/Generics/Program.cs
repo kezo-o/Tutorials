d1629a9f0220c1567a9a3505d8ddf11ff7f84d4b
HansaJugnarain <48100071+HansaJugnarain@users.noreply.github.com>
2023-03-22 21:21:38 +0400

    Exercise 1 rewrite the AreEqual method

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -15,7 +15,16 @@ namespace Generics
             IsEqual = ClsCalculator.AreEqual(10, 20);
             PrintMessage(IsEqual);
             IsEqual = ClsCalculator.AreEqual<string>("ABC", "ABC");
+            PrintMessage(IsEqual);
             IsEqual = ClsCalculator.AreEqual<double>(10.5, 20.5);
+            PrintMessage(IsEqual);
+
+            Console.WriteLine($"Test returns : {ClsCalculator.Test<string>(10)}");
+
+            Console.WriteLine($"Test returns : {ClsCalculator.Test<bool>(20)}");
+
+            Console.WriteLine($"Test returns : {ClsCalculator.Test<double>(30)}");
+
 
             Console.ReadKey();
 
@@ -36,9 +45,17 @@ namespace Generics
     //using generics to make all 3 methods work
     public class ClsCalculator
     {
-        public static bool AreEqual(int value1, int value2)
+        public static bool AreEqual<T>(T value1, T value2)
         {
-            return value1 == value2;
+            Console.WriteLine($"Value 1 : {value1}, Value 2 : {value2}");
+            return value1.Equals(value2);
+        }
+
+        //Return type of method generic
+        public static T Test<T>(int val)
+        {
+            Console.WriteLine($"Test - Integer : {val}, Type of method : {typeof(T)} ");
+            return (T)Convert.ChangeType(val, typeof(T));
         }
     }
 }
